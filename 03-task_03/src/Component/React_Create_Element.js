7cce6e8590c39c14882e4bbb88a760ebc73adaa8
Aqsa Siddiqui <112504820+aqsasiddiqui19@users.noreply.github.com>
2023-03-27 01:27:26 +0500

    # Task-03 Done

---
+++ 03-task_03/src/Component/React_Create_Element.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Article = () => {
+   return 
+   
+   <div className = 'article'>
+     <h1>Article</h1>
+   <p>Tackle food waste to fix our food systems
+   </p>
+   </div>
+}
+
+//return React.createElement(
+//    'article',
+//    {id:'article'},
+
+//React.createElement("p" , null,'Tackle food waste to fix our food systems'),);}
+
+
+export default Article
